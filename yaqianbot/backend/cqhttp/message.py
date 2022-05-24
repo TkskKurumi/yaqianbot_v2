30f3a2d1c3b5f68b55ae3f2415fc71e022e03544
TkskKurumi <zafkielkurumi@gmail.com>
2022-05-24 02:20:11 -0400

    New: send image

--- yaqianbot/backend/cqhttp/message.py
+++ yaqianbot/backend/cqhttp/message.py
@@ -1,11 +1,76 @@
+
 from typing import Dict
 from aiocqhttp import Event
 import re
-
+from aiocqhttp.message import MessageSegment as MSEG
 from ..bot_threading import threading_run
 from ..base_message import User, Message
 from .. import requests
 from . import cqhttp
+from os import path
+from PIL import Image
+from math import sqrt
+from ..paths import temppth
+import base64
+from io import BytesIO
+def img_b64(im, limit_size = (2<<20)):
+    
+    if("A" in im.mode):
+        format = "PNG"
+    else:
+        format = "JPEG"
+    w, h = im.size
+    bio, cur_size = None,None
+    def _save():
+        nonlocal bio, im, format, cur_size
+        bio = BytesIO()
+        im.save(bio, format)
+        cur_size = bio.tell()
+        return cur_size
+    while(_save()>limit_size):
+        r = 0.85*min(1, sqrt(limit_size/cur_size))
+        w, h=int(w*r),int(h*r)
+    
+    bio.seek(0)
+    bytes = bio.read()
+    bio.close()
+    return base64.b64encode(bytes).decode("ascii")
+
+def img_file_b64(filename, limit_size = (2<<20)):
+    im = Image.open(filename)
+    if(im.mode not in ["RGB", "RGBA"]):
+        with open(filename,"rb") as f:
+            ret = base64.b64encode(f.read()).decode("ascii")
+        return ret
+    else:
+        return img_b64(im, limit_size)
+def prepare_message(mes):
+    if(not isinstance(mes, list)):
+        mes = [mes]
+    ret = []
+    for i in mes:
+        if(isinstance(i, str)):
+            if(path.exists(i)):
+                ext = path.splitext(i)[-1].lower()
+                if(ext in [".jpg", ".gif", ".bmp", ".png"]):
+                    file = "base64://"+img_file_b64(i)
+                    mes = MSEG.image(file)
+                    ret.append(mes)
+                elif(ext in [".wav",".mp3",".ogg"]):
+                    mes = MSEG.record(i)
+                    ret.append(mes)
+                else:
+                    ret.append(MSEG.text(i))
+            else:
+                ret.append(MSEG.text(i))
+        elif(isinstance(i, Image.Image)):
+            file = "base64://"+img_b64(i)
+            ret.append(MSEG.image(file))
+        elif(isinstance(i, MSEG) or isinstance(i, dict)):
+            ret.append(i)
+        else:
+            ret.append(MSEG.text(str(i)))
+    return ret
 
 def mes_str2arr(message):
     pattern = r"(\[CQ:(.+?),(.+?)\])"
@@ -64,7 +129,8 @@ class CQMessage(Message):
                 pics.append(i)
             elif(type == "at"):
                 ated.append(data.get("qq"))
-        ret = cls(sender=sender, pics=pics, ated=ated, plain_text = event.raw_message, group=group, raw = event)
+        ret = cls(sender=sender, pics=pics, ated=ated,
+                  plain_text=event.raw_message, group=group, raw=event)
         ret.update_rpics()
         return ret
 
@@ -77,26 +143,29 @@ class CQMessage(Message):
             im = requests.get_image(url)
             ret.append(im)
         return ret
+
     @threading_run
     def response_sync(self, message, at=False, reply=False):
         args = dict()
+
         def prepare(**kwargs):
             args.update(kwargs)
-        for key in ["message_type","group_id","user_id","self_id"]:
+        for key in ["message_type", "group_id", "user_id", "self_id"]:
             if(key in self.raw):
-                args[key]=self.raw[key]
-        prepare(message = message)
-        print(args)
+                args[key] = self.raw[key]
+        prepare(message=prepare_message(message))
+        # print(args)
         ret = cqhttp._bot.sync.send_msg(**args)
-        
+
     async def response_async(self, message, at=False, reply=False):
         args = dict()
+
         def prepare(**kwargs):
             args.update(kwargs)
-        for key in ["message_type","group_id","user_id","self_id"]:
+        for key in ["message_type", "group_id", "user_id", "self_id"]:
             if(key in self.raw):
-                args[key]=self.raw[key]
-        prepare(message = message)
-        print(args)
+                args[key] = self.raw[key]
+        prepare(message=prepare_message(message))
+        # print(args)
         ret = await cqhttp._bot.send_msg(**args)
         return ret
