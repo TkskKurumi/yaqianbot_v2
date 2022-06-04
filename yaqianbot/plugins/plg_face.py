032ffbfff5e4b965ca79c9b62f260540bafb7732
TkskKurumi <zafkielkurumi@gmail.com>
2022-06-04 08:07:46 -0400

    plugins/plg_face newe: /要我一直 utils.image new: extract gif frames, colors interpolation

--- yaqianbot/plugins/plg_face.py
+++ yaqianbot/plugins/plg_face.py
@@ -7,7 +7,46 @@ from datetime import timedelta
 import numpy as np
 from PIL import Image
 from ..utils import image
+from ..utils.image.sizefit import fit_shrink, fix_width
+from ..utils.image import gif_frames_duration
+from pil_functional_layout.widgets import Column, RichText, Text
+from ..utils.make_gif import make_gif
+'''
+@receiver
+@threading_run
+@startswith("/要我一直gif")
+def cmd_ywyz_gif(message: CQMessage):
+    img = message.get_sent_images()[0][1].convert("RGB")
+    img = fit_shrink(img, 500, 500)
+    w, h = img.size
+    text1 = Text("要我一直", fontSize=36, bg=(255,)*4).render()
+    small_image = fit_shrink(img, height=36)
+    text2 = Text("吗", fontSize=36, bg=(255,)*4).render()
+    w, h = max(img.size[0], sum([i.size[0]
+               for i in [text1, small_image, text2]]))
+    h = img.size[0]+max([i.size[1] for i in [text1, small_image, text2]])'''
 
+def pic_ywyz(img):
+    img = fix_width(img, 500)
+    fontSize = 36
+    rt = RichText(["要我一直", fit_shrink(img, height=fontSize),
+                  "吗？"], fontSize=fontSize, width=500, autoSplit=False, dont_split=True)
+    ret = Column([img, rt], bg=(255,)*3).render()
+    return ret
+@receiver
+@threading_run
+@startswith("/要我一直")
+def cmd_ywyz(message: CQMessage):
+    imgtype, img = message.get_sent_images()[0]
+    if(imgtype == "image/gif"):
+        frames, fps = image.gif_frames_fps(img)
+        retframes = [pic_ywyz(i) for i in frames]
+        gif = make_gif(retframes, fps=fps, frame_area_sum = 1e6)
+        message.response_sync(gif)
+    else:
+        ret = pic_ywyz(img)
+        message.response_sync(ret)
+    
 @receiver
 @threading_run
 @startswith("/群青")
@@ -15,6 +54,11 @@ def cmd_gunjou(message: CQMessage):
     img = message.get_sent_images()[0][1].convert("RGB")
     arr = np.asarray(img)
     gray = arr.mean(axis=2, keepdims=False)
-    out_arr=image.np_colormap(gray,[[0,0,0],[0,0,255],[100,155,255],[255,255,255]])
+    colors = []
+    colors.append((0, 0, 0))
+    colors.append((0, 0, 255))
+    colors.append((100, 155, 255))
+    colors.append((255, 255, 255))
+    out_arr = image.np_colormap(gray, colors)
 
     message.response_sync(Image.fromarray(out_arr.astype(np.uint8)))
--- yaqianbot/utils/image/__init__.py
+++ yaqianbot/utils/image/__init__.py
@@ -4,6 +4,7 @@ import numpy as np
 def image_is_dark(img):
     arr = np.array(img.convert("L"))
     return np.mean(arr)<128
+
 def gif_frames_duration(gifimg):
     frm = 0
     ret = []
@@ -11,8 +12,12 @@ def gif_frames_duration(gifimg):
     while(True):
         try:
             gifimg.seek(frm)
+            frm+=1
         except EOFError:
             break
         duration+=gifimg.info["duration"]
         ret.append(gifimg.convert("RGBA"))
     return ret, duration
+def gif_frames_fps(gifimg):
+    frames, dur = gif_frames_duration(gifimg)
+    return frames, len(frames)/(dur/1000)
