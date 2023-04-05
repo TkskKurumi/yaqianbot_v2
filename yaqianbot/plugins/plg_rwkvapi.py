78f6fb61174d558d1da6779e8e26d165342d85a1
TkskKurumi <zafkielkurumi@gmail.com>
2023-04-05 21:54:43 +0800

    Adjust Chat initial prompt.

--- yaqianbot/plugins/plg_rwkvapi.py
+++ yaqianbot/plugins/plg_rwkvapi.py
@@ -43,20 +43,18 @@ def do_cont(message: CQMessage, prompt, from_state="new", recall=None):
             recall=recall
         )
 
-def do_chat(message: CQMessage, prompt, from_state="new", username="User", botname="Bot", sep=":"):
+def do_chat(message: CQMessage, prompt, from_state="new", username="Bob", botname="Alice", sep=":"):
     global chat_args, cont_args
     if(from_state=="new"):
         feed_prefix = f"""
-The following is a coherent verbose detailed conversation between {botname} and her friend {username}. \
+The following is a coherent verbose detailed conversation between a Chinese girl named {botname} and her friend {username}. \
 {botname} is very intelligent, creative and friendly. \
 {botname} likes to tell {username} a lot about herself and her opinions. \
 {botname} usually gives {username} kind, helpful and informative advices.
-{username}{sep} 你好，你是谁？
-{botname}{sep} 我是{botname}。很高兴认识你。
-{username}{sep} 我可以问你几个问题吗？
-{botname}{sep} 当然，乐意效劳。
-{username}{sep} 今天天气如何？
-{botname}{sep} 我不知道您处在哪个城市？或许您可以自行上网查询一下当地天气。
+{username}{sep} lhc
+{botname}{sep} LHC是指大型强子对撞机（Large Hadron Collider），是世界最大最强的粒子加速器，由欧洲核子中心（CERN）在瑞士日内瓦地下建造。LHC的原理是加速质子（氢离子）并让它们相撞，让科学家研究基本粒子和它们之间的相互作用，并在2012年证实了希格斯玻色子的存在。
+{username}{sep} 企鹅会飞吗
+{botname}{sep} 企鹅是不会飞的。企鹅的翅膀短而扁平，更像是游泳时的一对桨。企鹅的身体结构和羽毛密度也更适合在水中游泳，而不是飞行。
 """
     else:
         feed_prefix=""
@@ -75,7 +73,10 @@ The following is a coherent verbose detailed conversation between {botname} and 
     r = requests.post(url, json=data)
     j = r.json()
     if(j["status"]!=0):
-        simple_send(j["message"])
+        mes = [j["message"]]
+        if(j["status"] == -404):
+            mes.append("服务端返回会话不存在, 请在指令后加入 -reset 来取消使用上一次对话的状态。")
+        simple_send("\n".join(mes))
         return
     data = j["data"]
     contents = data["contents"]
